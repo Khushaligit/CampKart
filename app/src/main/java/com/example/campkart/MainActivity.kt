6e4e5c4eb268be2009bc99cafec8edbddfb58ea5
Atharva <atharvakokil14012002@gmail.com>
2026-03-11 13:08:41 +0530

    Atharva Push

--- app/src/main/java/com/example/campkart/MainActivity.kt
+++ app/src/main/java/com/example/campkart/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! this is new name Helooo Ankush",
+        text = "Hello $name! this is new name Helooo Atharva",
 
         modifier = modifier
     )
