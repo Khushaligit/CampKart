ed393a1c5e1d2e47a4ab76cb624c13095f038c9c
Mayank <sethmayank33@gmail.com>
2026-03-11 12:56:06 +0530

    mayank push

--- app/src/main/java/com/example/campkart/MainActivity.kt
+++ app/src/main/java/com/example/campkart/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! this is new name",
+        text = "Hello $name! this is new name Helooo",
 
         modifier = modifier
     )
