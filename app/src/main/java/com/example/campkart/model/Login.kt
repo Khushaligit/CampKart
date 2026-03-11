a5da37a23dc371e785fe021ef8c2199e97779208
Mayank <sethmayank33@gmail.com>
2026-03-12 00:14:54 +0530

    view model

---
+++ app/src/main/java/com/example/campkart/model/Login.kt
@@ -0,0 +1,6 @@
+package com.example.campkart.model
+
+data class Login(
+    val userId: String="",
+    val userPassword: String=""
+)
--- app/src/main/java/com/example/campkart/model/Users.kt
+++ app/src/main/java/com/example/campkart/model/Users.kt
@@ -1,8 +1,8 @@
 package com.example.campkart.model
 
 data class Users(
-    var campusName: String,
-    var userId: String,
-    var userPassword: String,
-    var userContact: String
+    val id: String="",
+    val campusName: String="",
+    val userId: String="",
+    val userContact: String=""
 )
